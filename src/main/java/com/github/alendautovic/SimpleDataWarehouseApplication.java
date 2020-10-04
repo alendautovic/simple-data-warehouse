a616a5ffbd50fe99115a4fbeee9333ca0833c693
Alen Dautović <alen.dautovic@amplitudo.me>
2020-10-04 10:33:16 +0200

    Project initialized

---
+++ src/main/java/com/github/alendautovic/SimpleDataWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.github.alendautovic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDataWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDataWarehouseApplication.class, args);
+	}
+
+}
